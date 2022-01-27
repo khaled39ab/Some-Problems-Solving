274c3ee587272ac6cc4bc9381915bbf19f0119c6
khaled39ab <khaled39ab@gmail.com>
2022-01-27 22:24:30 +0600

    sorting

---
+++ sorting.js
@@ -0,0 +1,5 @@
+// Student name sorting by alphabet 
+
+const student = [ "Jamaan", "Kalam", "Kobir", "Amir", "Sakib", "Jolil", "Aman", "Atik", "Amin", "Bokul", "Delowar", "Amiin"];
+
+console.log(student.sort());
