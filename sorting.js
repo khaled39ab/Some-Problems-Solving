5f510cc3cce2a43a8267e916979019044cc1d6ca
khaled39ab <khaled39ab@gmail.com>
2022-01-27 23:37:21 +0600

    sorting

--- sorting.js
+++ sorting.js
@@ -1,5 +1,11 @@
 // Student name sorting by alphabet 
-
-const student = [ "Jamaan", "Kalam", "Kobir", "Amir", "Sakib", "Jolil", "Aman", "Atik", "Amin", "Bokul", "Delowar", "Amiin"];
-
+const student = [ "Jamaan", "Kalam", "Kobir", "Amir", "Sakib", "Jolil", "Aman", "Atik", "Amin",  "Delowar", "Amiin"];
 console.log(student.sort());
+
+// Student Roll number sorting
+const roll = [25, 45, 14, 23, 01, 19, 09, 75, 26, 04, 31, 11, 03, 17,21];
+// const roll_Num = roll.sort( (a, b) => { return a - b});
+const roll_Num = roll.sort(function(a, b){return a - b});
+console.log(roll_Num);
+
+
