938fc11652d99868379bbff5c35db1742d506fb9
khaled39ab <khaled39ab@gmail.com>
2022-10-08 21:03:43 +0600

    sorting problem

--- sorting.js
+++ sorting.js
@@ -9,3 +9,35 @@ const roll_Num = roll.sort(function(a, b){return a - b});
 console.log(roll_Num);
 
 
+// sorting with for loops
+const num = [13, 17, 14, 19, 16];
+let temp = 0;
+for (let i = 0; i < num.length; i++) {
+    for (let j = i + 1; j < num.length; j++) {
+        if (num[i] > num[j]) {
+            temp = num[i]
+            num[i] = num[j]
+            num[j] = temp
+        }
+    }
+}
+
+// console.log(num);
+
+/* 
+---------------------------------------   sorting with sort function
+*/
+
+const number = [43, 54, 23, 65, 75, 21, 32]
+number.sort(function (a, b) {
+    return a - b
+})
+
+// console.log(number);
+
+// ----------------   reverse sorting
+number.sort(function (a, b) {
+    return b - a
+})
+
+console.log(number);
